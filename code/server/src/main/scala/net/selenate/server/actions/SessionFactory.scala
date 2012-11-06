d433171e7b46d99c31524e18b43d0bec18eef933
Tomo Ferega <tomo@element.hr>
2012-11-06 11:07:47 +0100

    Added support for reusing sessions.

--- code/server/src/main/scala/net/selenate/server/actions/SessionFactory.scala
+++ code/server/src/main/scala/net/selenate/server/actions/SessionFactory.scala
@@ -5,8 +5,17 @@ import akka.actor.ActorRef
 
 class SessionFactory extends ISessionFactory {
   def getSession(sessionID: String): String = {
-    val name = "session-actor-"+ sessionID
-    ActorFactory.untyped(name, () => new SessionActor(sessionID))
-    name
+    println("GOT A REQUEST FOR SESSION: "+ sessionID)
+    SessionCache.get(sessionID) match {
+      case None =>
+        println("SESSION NOT FOUND IN CACHE; CREATING")
+        val name = sessionID
+        val session = ActorFactory.untyped(name, () => new SessionActor(sessionID))
+        SessionCache(sessionID) = session
+        session.path.name
+      case Some(session) =>
+        println("SESSION FOUND IN CACHE; REUSING")
+        session.path.name
+    }
   }
 }
