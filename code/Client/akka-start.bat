6b200b0bb861d76515f417529c893a45b52bbfc9
Tomo Ferega <tomo@element.hr>
2012-12-03 12:17:46 +0100

    Fixed akka-start scripts.

--- code/Client/akka-start.bat
+++ code/Client/akka-start.bat
@@ -1,4 +1,4 @@
 @echo off
 
 echo Starting up Akka - ELVIS HAS LEFT THE BUILDING ...
-call "%~dp0sbt.bat" --loop %* "~run-main net.selenate.server.client.EntryPoint"
+call "%~dp0sbt.bat" --loop %* "~run-main net.selenate.client.EntryPoint"
