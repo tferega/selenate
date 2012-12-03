215decc031d0ba5f70b84131bce831a7c2ea7c11
Tomo Ferega <tomo@element.hr>
2012-12-03 15:47:01 +0100

    Created an implementation for IElement.

---
+++ code/Client/src/main/java/net/selenate/client/user/ActorElement.java
@@ -0,0 +1,154 @@
+package net.selenate.client.user;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import akka.actor.ActorRef;
+
+import net.selenate.common.user.IElement;
+import net.selenate.common.user.ISelect;
+import net.selenate.common.user.Location;
+import net.selenate.common.user.Position;
+
+public class ActorElement implements IElement {
+  protected final ActorRef session;
+
+  private final String   uuid;
+  private final Position pos;
+  private final Location loc;
+  private final String   name;
+  private final String   text;
+  private final boolean  isDisplayed;
+  private final boolean  isEnabled;
+  private final boolean  isSelected;
+  private final Map<String, String> attributeList;
+  private final List<IElement>      children;
+
+  public ActorElement(
+      final ActorRef session,
+      final String   uuid,
+      final Position pos,
+      final Location loc,
+      final String   name,
+      final String   text,
+      final boolean  isDisplayed,
+      final boolean  isEnabled,
+      final boolean  isSelected,
+      final Map<String, String> attributeList,
+      final List<IElement>      children) {
+    if (session == null) {
+      throw new IllegalArgumentException("Session cannot be null!");
+    }
+    if (uuid == null) {
+      throw new IllegalArgumentException("UUID cannot be null!");
+    }
+    if (pos == null) {
+      throw new IllegalArgumentException("Position cannot be null!");
+    }
+    if (loc == null) {
+      throw new IllegalArgumentException("Location cannot be null!");
+    }
+    if (name == null) {
+      throw new IllegalArgumentException("Name cannot be null!");
+    }
+    if (text == null) {
+      throw new IllegalArgumentException("Text cannot be null!");
+    }
+    if (attributeList == null) {
+      throw new IllegalArgumentException("Attribute list cannot be null!");
+    }
+    if (children == null) {
+      throw new IllegalArgumentException("Children cannot be null!");
+    }
+
+    this.session       = session;
+    this.uuid          = uuid;
+    this.pos           = pos;
+    this.loc           = loc;
+    this.name          = name;
+    this.text          = text;
+    this.isDisplayed   = isDisplayed;
+    this.isEnabled     = isEnabled;
+    this.isSelected    = isSelected;
+    this.attributeList = attributeList;
+    this.children      = children;
+  }
+
+
+  @Override
+  public String getUuid() {
+    return uuid;
+  }
+
+  @Override
+  public Position getPos() {
+    return pos;
+  }
+
+  @Override
+  public Location getLoc() {
+    return loc;
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String getText() {
+    return text;
+  }
+
+  @Override
+  public boolean getIsDisplayed() {
+    return isDisplayed;
+  }
+
+  @Override
+  public boolean getIsEnabled() {
+    return isEnabled;
+  }
+
+  @Override
+  public boolean getIsSelected() {
+    return isSelected;
+  }
+
+  @Override
+  public Map<String, String> getAttributeList() {
+    return attributeList;
+  }
+
+  @Override
+  public List<IElement> getChildren() {
+    return children;
+  }
+
+  @Override
+  public ISelect toSelect() {
+    throw new IllegalArgumentException("Not supported");
+  }
+
+  @Override
+  public void clearText() throws IOException {
+    throw new IllegalArgumentException("Not supported");
+  }
+
+  @Override
+  public void appendText() throws IOException {
+    throw new IllegalArgumentException("Not supported");
+  }
+
+  @Override
+  public void setText() throws IOException {
+    throw new IllegalArgumentException("Not supported");
+  }
+
+  @Override
+  public void click() throws IOException {
+    throw new IllegalArgumentException("Not supported");
+  }
+}
