7d383869babe8d0a2cbb73562a70928a1026560f
Ana Marjanica <ana@element.hr>
2013-09-03 14:11:14 +0200

    added capture without screenshot

--- code/Common/src/main/java/net/selenate/common/comms/req/SeReqCapture.java
+++ code/Common/src/main/java/net/selenate/common/comms/req/SeReqCapture.java
@@ -4,6 +4,7 @@ public class SeReqCapture implements SeCommsReq {
   private static final long serialVersionUID = 1L;
 
   public final String name;
+  public boolean takeScreenshot = true;
 
   public SeReqCapture(final String name) {
     if (name == null) {
@@ -13,8 +14,18 @@ public class SeReqCapture implements SeCommsReq {
     this.name = name;
   }
 
+  public SeReqCapture(final String name, final boolean takeScreenshot) {
+    if (name == null) {
+      throw new IllegalArgumentException("name cannot be null!");
+    }
+
+    this.name = name;
+    this.takeScreenshot = takeScreenshot;
+  }
+
+
   @Override
   public String toString() {
-    return String.format("SeReqCapture(%s)", name);
+    return String.format("SeReqCapture(%s, %s)", name, takeScreenshot);
   }
 }
