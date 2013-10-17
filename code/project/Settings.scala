f9b54cef036353500ba9b08bda56f1db4b658b96
Davor Stimac <davor.stimac@gmail.com>
2013-10-17 18:15:10 +0200

    VERSION BUMP => 0.2.6

--- code/project/Settings.scala
+++ code/project/Settings.scala
@@ -74,7 +74,7 @@ object Default {
     Seq(
       name         := Name,
       organization := "net.selenate",
-      version      := "0.2.5",
+      version      := "0.2.6",
       scalaVersion := "2.10.2",
       unmanagedSourceDirectories in Test := Nil
   )
