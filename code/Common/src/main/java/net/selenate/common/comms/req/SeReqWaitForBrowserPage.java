4ca889cd494174771308facc06a146ba33c4870c
Davor Stimac <davor.stimac@gmail.com>
2013-12-23 16:10:30 +0100

    ** NEW SELENATE VERSION 0.2.14
    * added waitForAnyPage to ActorBrowser/IBrowser
    * implemented waitForAnyPage in selenate-server (returns found BrowserPage object or null if not found)
    * added hashCode() and equals() to BrowserPage
    * BrowserPage now implements Serializable
    * ElementSelector now implements Serializable
    * added reqSelectMethodToUserSelectMethdod converter to ActionCommons
    * added WaitForBrowserPageAction
    
    Signed-off-by: Davor Stimac <davor.stimac@gmail.com>

---
+++ code/Common/src/main/java/net/selenate/common/comms/req/SeReqWaitForBrowserPage.java
@@ -0,0 +1,38 @@
+package net.selenate.common.comms.req;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.selenate.common.comms.*;
+import net.selenate.common.util.Util;
+
+public class SeReqWaitForBrowserPage implements SeCommsReq {
+  private static final long serialVersionUID = 1L;
+
+  public final List<SePage> pageList;
+
+  public SeReqWaitForBrowserPage(final List<SePage> pageList) {
+    if (pageList == null) {
+      throw new IllegalArgumentException("Page list cannot be null!");
+    }
+    if (pageList.isEmpty()) {
+      throw new IllegalArgumentException("Page list cannot be empty!");
+    }
+
+    this.pageList = pageList;
+  }
+
+  public SeReqWaitForBrowserPage(final SePage page) {
+    if (page == null) {
+      throw new IllegalArgumentException("Page cannot be null!");
+    }
+
+    this.pageList = new ArrayList<SePage>();
+    this.pageList.add(page);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("SeReqWaitForBrowserPage()%s", Util.multilineListToString(pageList));
+  }
+}
--- code/Common/src/main/java/net/selenate/common/user/BrowserPage.java
+++ code/Common/src/main/java/net/selenate/common/user/BrowserPage.java
@@ -1,8 +1,11 @@
 package net.selenate.common.user;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class BrowserPage {
+public class BrowserPage implements Serializable{
+  private static final long serialVersionUID = -4227576752950157494L;
+
   public String name;
   public List<ElementSelector> selectorList;
 
@@ -21,5 +24,37 @@ public class BrowserPage {
 
     this.name         = name;
     this.selectorList = selectorList;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result
+        + ((selectorList == null) ? 0 : selectorList.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    BrowserPage other = (BrowserPage) obj;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (selectorList == null) {
+      if (other.selectorList != null)
+        return false;
+    } else if (!selectorList.equals(other.selectorList))
+      return false;
+    return true;
   }
 }
