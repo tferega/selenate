4e94635b5a1f4f136ff9d58fce256dbbf187b422
Davor Stimac <davor.stimac@gmail.com>
2014-03-04 11:21:24 +0100

    * Session creation with useFrame parameter

--- code/Common/src/main/java/net/selenate/common/sessions/ISessionFactory.java
+++ code/Common/src/main/java/net/selenate/common/sessions/ISessionFactory.java
@@ -9,4 +9,6 @@ import scala.concurrent.Future;
 public interface ISessionFactory {
   public Future<ActorRef> getSession(final String sessionID, final Preferences Preferences);
   public Future<ActorRef> getSession(final String sessionID);
+  public Future<ActorRef> getSession(final String sessionID, final Preferences Preferences, final Boolean useFrames);
+  public Future<ActorRef> getSession(final String sessionID, final Boolean useFrames);
 }
