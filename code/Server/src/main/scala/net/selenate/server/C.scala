e4f533df7ee7ec0487cd1c7a390ca071ae6c61c7
Davor Stimac <davor.stimac@gmail.com>
2014-07-23 17:12:35 +0200

    config load fix
    Signed-off-by: Davor Stimac <davor.stimac@gmail.com>

--- code/Server/src/main/scala/net/selenate/server/C.scala
+++ code/Server/src/main/scala/net/selenate/server/C.scala
@@ -7,11 +7,15 @@ import scala.io.Source
 
 object C {
   val branch             = sys.props("branch")
-  private val configPath = sys.props("user.home") / ".config" / "selenate" / branch / "server.config"
+  private val configPath = sys.props("user.home") + "/.config" + "/selenate/" + branch + "/server.config"
   private val config     = Configuration.load(configPath)
 
   object Server {
     private val serverConfig = config.detach ("server")
+    val poolSize          = serverConfig[Int]("pool-size")
+    val host              = serverConfig[String]("host")
+    //val defaultProfileOpt = serverConfig.get[String]("default-profile")
+
 
     object Pool {
       private val poolConfig = serverConfig.detach("pool")
