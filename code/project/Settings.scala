696e146c34715b781f06fefae2cf8aa5f0098428
Marko Dolancic <marko@dolancic.net>
2015-05-06 15:06:28 +0200

    Add case value (x86_64) to Settings.platform. Without it, project will not compile on MacOS.

--- code/project/Settings.scala
+++ code/project/Settings.scala
@@ -152,7 +152,7 @@ object Helpers {
     val osArch =
       osArchProp match {
       case "i386" | "i486" | "i586" | "i686" => "x86"
-      case "amd64" | "x86-64" | "x64"        => "x86_64"
+      case "amd64" | "x86-64" | "x64" | "x86_64" => "x86_64"
     }
 
     osName + "-" + osArch
