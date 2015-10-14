d28532089e9affe20d1e7d0dfbde641d9efe24c8
Tomo Ferega <tomo@element.hr>
2015-10-14 19:11:35 +0200

    Updated dependencies

--- code/project/Dependencies.scala
+++ code/project/Dependencies.scala
@@ -2,24 +2,24 @@ import sbt._
 import Keys._
 
 trait Dependencies {
-  val akkaVersion = "2.3.9"
+  val akkaVersion = "2.4.0"
   val akkaActor   = "com.typesafe.akka" %% "akka-actor"  % akkaVersion
   val akkaRemote  = "com.typesafe.akka" %% "akka-remote" % akkaVersion
   val akkaSlf4j   = "com.typesafe.akka" %% "akka-slf4j"  % akkaVersion
 
-  val jodaTimeConvert = "org.joda"  % "joda-convert" % "1.7"
-  val jodaTimeTime    = "joda-time" % "joda-time"    % "2.7"
+  val jodaTimeConvert = "org.joda"  % "joda-convert" % "1.8.1"
+  val jodaTimeTime    = "joda-time" % "joda-time"    % "2.8.2"
 
-  val seleniumVersion = "2.44.0"
+  val seleniumVersion = "2.48.2"
   val seleniumFirefox = "org.seleniumhq.selenium" % "selenium-firefox-driver" % seleniumVersion
   val seleniumServer  = "org.seleniumhq.selenium" % "selenium-server"         % seleniumVersion
 
   // ---------------------------------------------------------------------------
   
-  val config           = "com.typesafe"            %  "config"               % "1.2.1"
-  val dispatch         = "net.databinder.dispatch" %% "dispatch-core"        % "0.11.2"
-  val logback          = "ch.qos.logback"          %  "logback-classic"      % "1.1.2"
-  val slf4j            = "org.slf4j"               %  "slf4j-api"            % "1.7.10"
+  val config           = "com.typesafe"            %  "config"               % "1.3.0"
+  val dispatch         = "net.databinder.dispatch" %% "dispatch-core"        % "0.11.3"
+  val logback          = "ch.qos.logback"          %  "logback-classic"      % "1.1.3"
+  val slf4j            = "org.slf4j"               %  "slf4j-api"            % "1.7.12"
   val procrun          = "com.ferega.procrun"      %% "processrunner"        % "0.1.3"
 
   val akka     = Seq(akkaActor, akkaRemote, akkaSlf4j)
--- code/project/plugins.sbt
+++ code/project/plugins.sbt
@@ -1,14 +1,4 @@
 // +-------------------------------------------------------------------------------------+
-// | SBT Eclipse (https://github.com/typesafehub/sbteclipse)                             |
-// | Creates .project and .classpath files for easy Eclipse project imports              |
-// |                                                                                     |
-// | See also: Eclipse downloads (http://www.eclipse.org/downloads/)                     |
-// | See also: Scala IDE downloads (http://download.scala-ide.org/)                      |
-// +-------------------------------------------------------------------------------------+
-
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "3.0.0")
-
-// +-------------------------------------------------------------------------------------+
 // | Dependency graph SBT plugin (https://github.com/jrudolph/sbt-dependency-graph)      |
 // | Lists all library dependencies in a nicely formatted way for easy inspection.       |
 // +-------------------------------------------------------------------------------------+
@@ -21,4 +11,4 @@ addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
 // | A sbt plugin for creating distributable Scala packages that include dependent jars  |
 // | and launch scripts.                                                                 |
 // +-------------------------------------------------------------------------------------+
-addSbtPlugin("org.xerial.sbt" % "sbt-pack" % "0.6.5")
+addSbtPlugin("org.xerial.sbt" % "sbt-pack" % "0.7.5")
