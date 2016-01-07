9d86a1fc8b5d6534f31caf6cc093cb79a048f8d8
Tomo Ferega <tomo@element.hr>
2016-01-07 14:57:36 +0100

    Removed logging to file

--- code/Server/src/main/scala/net/selenate/server/C.scala
+++ code/Server/src/main/scala/net/selenate/server/C.scala
@@ -22,7 +22,6 @@ object C extends CUtils {
 
     object Locations extends {
       val RECORDINGS = CONFIG.getString("server.locations.recordings")
-      val LOG        = CONFIG.getString("server.locations.log")
     }
 
     object Timeouts extends {
--- code/Server/src/main/scala/net/selenate/server/EntryPoint.scala
+++ code/Server/src/main/scala/net/selenate/server/EntryPoint.scala
@@ -1,22 +1,5 @@
 package net.selenate.server
 
-import ch.qos.logback.classic.LoggerContext
-import ch.qos.logback.classic.util.ContextInitializer
-import ch.qos.logback.core.util.StatusPrinter
-import org.slf4j.LoggerFactory
-
-object EntryPoint extends App with CBase {
-  initLog
+object EntryPoint extends App {
   Runner.start
-
-  def initLog() {
-    val logLocation = C.Server.Locations.LOG
-    System.setProperty("LOG_LOCATION", logLocation)
-
-    val lc = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
-    val ci = new ContextInitializer(lc);
-    lc.reset();
-    ci.autoConfig();
-    StatusPrinter.printInCaseOfErrorsOrWarnings(lc);
-  }
 }
